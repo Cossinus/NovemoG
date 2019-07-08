bd04724962e58e61c492390a4ea8472638811dd2
Cossinus <44206036+Cossinus@users.noreply.github.com>
2019-07-08 14:20:08 +0200

    Movement + Interaction

---
+++ Assets/Scripts/Player/Interactable.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 3f;
+    public Transform interactionTransform;
+
+    private bool isFocus = false;
+    private Transform player;
+
+    private bool hasInteracted = false;
+
+    public virtual void Interact()
+    {
+        // This is meant to be overwritten
+        Debug.Log("Interacting with " + transform.name);
+    }
+    
+    void Update()
+    {
+        if (isFocus && !hasInteracted)
+        {
+            float distance = Vector3.Distance(player.position, interactionTransform.position);
+            if (distance <= radius)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+    
+    public void OnFocused(Transform playerTransform)
+    {
+        isFocus = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+
+    public void OnDefocused()
+    {
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+    
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
+    }
+}
