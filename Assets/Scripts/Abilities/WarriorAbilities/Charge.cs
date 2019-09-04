d151a6c644b6b58913256f79a3c602352eac2764
Cossinus <44206036+Cossinus@users.noreply.github.com>
2019-09-04 21:08:38 +0200

    Charge rotation bugfix

--- Assets/Scripts/Abilities/WarriorAbilities/Charge.cs
+++ Assets/Scripts/Abilities/WarriorAbilities/Charge.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using Novemo;
 using UnityEngine;
@@ -7,8 +8,10 @@ namespace Novemo
 {
     public class Charge : Ability
     {
-        private Camera cam;
+        public float lookRadius = 7f;
         
+        private Camera cam;
+
         public override void Start()
         {
             base.Start();
@@ -17,17 +20,20 @@ namespace Novemo
             abilityDescription = "";
             cost = 5f;
             cooldown = 0.2f;
-            castTime = 0.5f;
+            castTime = 0.1f;
         }
 
         void Update()
         {
             Delay -= Time.deltaTime;
-            
-            if (Input.GetKey/*Down*/(KeyCode.Q))
+        }
+
+        private void LateUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
                 Use(cost, cooldown);
         }
-        
+
         protected override void Use(float cost, float cd)
         {
             if (Delay <= 0f && myStats.CurrentHealth > cost + 1)
@@ -43,22 +49,39 @@ namespace Novemo
         public override IEnumerator AAbility(float cd, string statName)
         {
             yield return new WaitForSeconds(castTime);
+            
+            Plane playerPlane = new Plane(Vector3.up, transform.position);
 
-            FaceDirection();
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            float hitdist;
+            if (playerPlane.Raycast(ray, out hitdist))
+            {
+                Vector3 targetPoint = ray.GetPoint(hitdist);
+                Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 100f * Time.deltaTime);
+
+                float distance = Vector3.Distance(targetPoint, transform.position);
+                
+                if (distance <= lookRadius)
+                {
+                    // Charge to the hitpoint
+                    // Stun every enemy hit and deal damage to them
+                    // focus nearest target and attack him once
+                }
+                else
+                {
+                    // Try going to the edge of lookRadius and then Charge
+                }
+            }
 
             yield return new WaitForSeconds(cd);
         }
-        
-        private void FaceDirection()
-        {
-            Vector2 mousePos = new Vector2();
 
-            mousePos.x = Input.mousePosition.x;
-            mousePos.y = Input.mousePosition.y;
-            
-            Vector3 direction = (-cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane))  - transform.position).normalized;
-            Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
-            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 25f);
+        void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, lookRadius);
         }
     }
 }
